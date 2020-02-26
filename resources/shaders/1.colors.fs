2c1fb2fda17ad6f75d73d42bc6549ffd0a0626bb
宝爷 <wyb10a10@163.com>
2020-02-26 17:55:51 +0800

    灯光变换+材质

--- resources/shaders/1.colors.fs
+++ resources/shaders/1.colors.fs
@@ -1,22 +1,48 @@
 #version 330 core
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+};
+
+struct Light {
+    vec3 position;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+uniform Light light;
+
+uniform Material material;
+
 out vec4 FragColor;
-  
-uniform vec3 objectColor;
-uniform vec3 lightColor;
+
 uniform vec3 lightPos;
+uniform vec3 viewPos;
 
 in vec3 Normal;
 in vec3 FragPos;
 
 void main()
 {
-    // 归一化光源方向和法线
+    // 环境光
+    vec3 ambient  = light.ambient * material.ambient;
+ 
+    // 漫反射
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
-    // 点乘，得到漫反射的光线强度（点乘是求2向量夹角的cos值，夹角越小光线越强，cos 0°时，点乘结果为1，向量垂直时，结果为0）
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
-    // 慢反射的光乘以物体本身的颜色，得到最终颜色
-    vec3 result = diffuse * objectColor;
+    vec3 diffuse  = light.diffuse * (diff * material.diffuse);
+    
+    // 镜面光
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = light.specular * (spec * material.specular);
+    
+    vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
 }
--- resources/shaders/1.colors.vs
+++ resources/shaders/1.colors.vs
@@ -5,6 +5,7 @@ layout (location = 1) in vec3 aNormal;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
+uniform vec3 viewPos;
 
 out vec3 Normal;
 out vec3 FragPos;
@@ -13,5 +14,6 @@ void main()
 {
     gl_Position = projection * view * model * vec4(aPos, 1.0);
     FragPos = vec3(model * vec4(aPos, 1.0)); // 输出片段的世界坐标到FragmentShader
-    Normal = aNormal;
+    // 通过法线矩阵计算法线位置，先取逆再转置，最好在外部算好用uniform传入
+    Normal = mat3(transpose(inverse(model))) * aNormal;
 }
